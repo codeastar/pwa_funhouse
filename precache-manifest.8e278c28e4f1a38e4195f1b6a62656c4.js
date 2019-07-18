28d98397bd9489ba37376e8a762d9d63080bc4d2
Raven Hon <hon@codeastar.com>
2019-07-19 00:32:57 +0800

    Updates

---
+++ precache-manifest.8e278c28e4f1a38e4195f1b6a62656c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a042b75f40f391cfced9292b90b45a4b",
+    "url": "/ez_react_pwa/index.html"
+  },
+  {
+    "revision": "f43bfc3bf1858ca42f5c",
+    "url": "/ez_react_pwa/static/css/main.61189c1a.chunk.css"
+  },
+  {
+    "revision": "3882842a8225619eef50",
+    "url": "/ez_react_pwa/static/js/2.a68de264.chunk.js"
+  },
+  {
+    "revision": "f43bfc3bf1858ca42f5c",
+    "url": "/ez_react_pwa/static/js/main.880d3cae.chunk.js"
+  },
+  {
+    "revision": "cb684dda260925063d7d",
+    "url": "/ez_react_pwa/static/js/runtime~main.117c0f0c.js"
+  },
+  {
+    "revision": "41d34f739d4de1f01be830b464a2a40b",
+    "url": "/ez_react_pwa/static/media/bitcoin.41d34f73.svg"
+  }
+]);
